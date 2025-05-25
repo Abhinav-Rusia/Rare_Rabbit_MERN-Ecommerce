37121afb697a76484e4f10f87d1ef4de08917693
Abhinav-Rusia <abhinavrusia@gmail.com>
2025-05-25 18:47:07 +0530

    fixed case sensitivity error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import ScrollToTop from "./components/common/ScrollToTop";
+import ScrollToTop from "./components/Common/ScrollToTop";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
